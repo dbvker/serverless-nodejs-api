15b13a5268a8cfcadcd557f8989c6a0e726fa45e
Dylan Baker <38574400+dbvker@users.noreply.github.com>
2025-07-13 22:02:13 +0000

    init

---
+++ src/handler.ts
@@ -0,0 +1,41 @@
+const serverless = require("serverless-http");
+const { neon, neonConfig } = require("@neondatabase/serverless");
+const express = require("express");
+import type { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+async function dbClient() {
+  neonConfig.fetchConnectionCache = true;
+  return neon(process.env.DATABASE_URL ?? "");
+}
+
+app.get("/", async ({ req, res, next }: ApiParamsType) => {
+  const sql = await dbClient();
+  const [results] = await sql`select now()`;
+  return res.status(200).json({
+    message: "Hello from root!",
+    results: results.now,
+  });
+});
+
+app.get("/hello", ({ req, res, next }: ApiParamsType) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use(({ req, res, next }: ApiParamsType) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+exports.handler = serverless(app);
+
+// Types
+type ApiParamsType = {
+  req: Request;
+  res: Response;
+  next?: NextFunction;
+};
